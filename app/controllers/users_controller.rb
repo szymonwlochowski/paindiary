39de1a8e5cfa880246a4c4e4f5c73f54430767ca
Szymon Włochowski <szymonwlochowski@gmail.com>
2014-12-21 18:09:57 +0000

    authenticate user on all controllers & expose user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,7 @@
 class UsersController < ApplicationController
 
-  def index
-    @users = User.all
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
+  expose(:user)
+  expose(:users)
 
   private
 
