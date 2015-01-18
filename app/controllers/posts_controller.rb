4fd2ed9b217016f20a9217b1c992cd2535588520
Szymon Włochowski <szymonwlochowski@gmail.com>
2015-01-18 22:24:07 +0000

    posts controller changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,54 +1,21 @@
 class PostsController < ApplicationController
-  before_filter :authenticate, :except => [ :index, :show ]
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  respond_to :html
-
-  def index
-    @posts = Post.all
-    respond_with(@posts)
-  end
-
-  def show
-    respond_with(@post)
-  end
-
-  def new
-    @post = Post.new
-    respond_with(@post)
-  end
-
-  def edit
-  end
+  expose(:post)
+  expose(:posts) { Post.order(:created_at).range(params[:period]).page(params[:page]) }
 
   def create
-    @post = Post.new(post_params)
-    @post.save
-    respond_with(@post)
+    post = Post.new(post_params)
+    post.save
   end
 
   def update
-    @post.update(post_params)
-    respond_with(@post)
-  end
-
-  def destroy
-    @post.destroy
-    respond_with(@post)
+    post.update(post_params)
   end
 
   private
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
     def post_params
       params.require(:post).permit(:title, :body, :description, :pain_level, :time, :comments, :meds, :non_drugs, {:description_ids => []})
     end
 
-    def authenticate
-      authenticate_or_request_with_http_basic do |name, password|
-        name == "admin" && password == "secret"
-    end
-  end
 end
