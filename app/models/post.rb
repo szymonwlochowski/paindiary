7e23148b5f7cf30d1c151ed22a83b03ff0cbc70e
Szymon Włochowski <szymonwlochowski@gmail.com>
2015-01-18 22:19:41 +0000

    scope for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,14 @@ class Post < ActiveRecord::Base
 
   validates :body, presence: true, length: { within: 10..10000000 }
   validates :title, presence: true, uniqueness: true
+
+  scope :range, ->(range) {
+    case range
+    when nil
+    when "week"
+      where{created_at.gteq 7.days.ago}
+    when "month"
+      where{created_at.gteq 1.month.ago}
+    end
+  }
 end
