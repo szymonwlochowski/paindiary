b078cdf0206599ae2258b5da2c597fb0427e7707
Szymon Włochowski <szymonwlochowski@gmail.com>
2015-03-31 00:42:58 +0100

    typos and mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Do care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -36,5 +36,17 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # an example of default_url_options appropriate for a development environment
-  config.action_mailer.default_url_options = { host: 'paindiary.dev', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'http://paindiary.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
