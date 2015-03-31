8b59017d70d89540c5ca46f78495e6a0119233db
Szymon Włochowski <szymonwlochowski@gmail.com>
2015-03-31 16:18:48 +0100

    mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: 'elo@paindiary.com'
+  default from: 'paindiarymails@gmail.com'
 
   def welcome_email(user)
     @user = user
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,12 +77,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.sendgrid.net",
     port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
+    domain: 'paindiary.herokuapp.com',
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"]
   }
 end
