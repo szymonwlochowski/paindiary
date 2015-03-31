6b66e9108998c68ca416b49b50c933db85eaddcf
Szymon Włochowski <szymonwlochowski@gmail.com>
2015-03-31 16:49:58 +0100

    domain change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: 587,
-    domain: 'paindiary.herokuapp.com',
+    domain: 'gmail.com',
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["SENDGRID_USERNAME"],
